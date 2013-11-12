7515459afab632de495a104cff381046300d1874
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-11-12 22:12:50 +0000

    Initial commit

---
+++ Fibonacci/JtEnumerable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    public static class JtEnumerable
+    {
+        // ReSharper disable FunctionNeverReturns
+        public static IEnumerable<long> Fibs()
+        {
+            var lastButOne = 0L;
+            var last = 1L;
+
+            for (var n = 0L;; n++)
+            {
+                if (n < 2) yield return n;
+                var next = lastButOne + last;
+                lastButOne = last;
+                last = next;
+                yield return next;
+            }
+        }
+        // ReSharper restore FunctionNeverReturns
+    }
+}
---
+++ FibonacciTests/FibonacciTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Fibonacci;
+using NUnit.Framework;
+
+namespace FibonacciTests
+{
+    [TestFixture]
+    class FibonacciTests
+    {
+        [Test]
+        public void TestFibsReturnsTheCorrectFirstTwelveValuesInTheSequence()
+        {
+            var fibonacciNumbers = JtEnumerable.Fibs().Take(12);
+            Assert.That(fibonacciNumbers, Is.EqualTo(new[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89}));
+        }
+
+        [Test]
+        public void TestFibsDoesNotWrapAroundForTheFirstNinetyThreeValuesInTheSequence()
+        {
+            var fibonacciNumbers = JtEnumerable.Fibs().Take(93).ToList();
+            Assert.That(fibonacciNumbers, Is.All.GreaterThanOrEqualTo(0));
+        }
+
+        [Test]
+        public void TestFibsDoesWrapAroundAtTheNinetyFourthValueInTheSequence()
+        {
+            var fibonacciNumbers = JtEnumerable.Fibs().Take(94).ToList();
+            Assert.That(fibonacciNumbers.Take(93), Is.All.GreaterThanOrEqualTo(0));
+            Assert.That(fibonacciNumbers.ElementAt(93), Is.LessThan(0));
+        }
+    }
+}
